6036343419161f8b08cbb56c98250d8bb5ab1db0
PraneethKumarManala <praneethmanala@gmail.com>
2019-01-31 14:50:57 +0530

    package is created in tha 2 classes are created

---
+++ FromSelenium/src/com/seleniu/tests/SecondClass.java
@@ -0,0 +1,11 @@
+package com.seleniu.tests;
+
+public class SecondClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Second Class");
+	}
+
+}
